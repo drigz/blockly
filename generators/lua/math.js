f8e814ab2b1d6861f7ed60e3b1c5a899435e959e
Rodrigo Queiro <rodrigoq@google.com>
2016-03-09 09:58:56 +0100

    Clean up header comments in Lua files
    Change copyright year to 2016 and use correct @author tag.

--- generators/lua/math.js
+++ generators/lua/math.js
@@ -2,7 +2,7 @@
  * @license
  * Visual Blocks Language
  *
- * Copyright 2012 Google Inc.
+ * Copyright 2016 Google Inc.
  * https://developers.google.com/blockly/
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,7 +20,7 @@
 
 /**
  * @fileoverview Generating Lua for math blocks.
- * @author q.neutron@gmail.com (Quynh Neutron)
+ * @author rodrigoq@google.com (Rodrigo Queiro)
  */
 'use strict';
 
--- generators/lua/variables.js
+++ generators/lua/variables.js
@@ -2,7 +2,7 @@
  * @license
  * Visual Blocks Language
  *
- * Copyright 2012 Google Inc.
+ * Copyright 2016 Google Inc.
  * https://developers.google.com/blockly/
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,7 +20,7 @@
 
 /**
  * @fileoverview Generating Lua for variable blocks.
- * @author fraser@google.com (Neil Fraser)
+ * @author rodrigoq@google.com (Rodrigo Queiro)
  */
 'use strict';
 
